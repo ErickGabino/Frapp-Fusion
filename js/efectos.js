f907e99274c8c8b2909bb3a0d0a1c968030e9766
ErickGabino <93292129+ErickGabino@users.noreply.github.com>
2023-04-21 16:46:23 -0600

    Add files via upload

---
+++ js/efectos.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+    //EFECTO NAV BAR
+    $('.menu a').each(function(index, elemento){
+        $(this).css({
+            'top': '-200px'
+        });
+        $(this).animate({
+            top:'0'
+        }, 2000 + (index * 500));
+    });
+
+    //EFECTO HEADER
+    if($(window).width() > 800){
+        $('header .textos').css({
+            opacity: 0,
+            marginTop: 0
+        });
+        $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        }, 1500);
+    }
+
+    //RESTABLECER
+    $(window).resize(function(){
+        if($(window).width() < 800){
+            $('header .textos').animate({
+                opacity: 1,
+                marginTop: '0px'
+            }, 1500);
+        }
+    });
+
+
+    //SCROLL ELEMENTOS MENU
+    var acerca = $('#acerca-de').offset().top,
+        menu = $('.contenedor-menu').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top,
+        resena = $('#resena').offset().top;
+
+    $('#btnAcerca').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: acerca-100
+        }, 500);
+    });
+
+    $('#btnMenu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu-100
+        }, 500);
+    });
+
+    $('#btnGaleria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500);
+    });
+
+    $('#btnUbicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500);
+    });
+
+    $('#btnResena').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: resena
+        }, 500);
+    });
+});
